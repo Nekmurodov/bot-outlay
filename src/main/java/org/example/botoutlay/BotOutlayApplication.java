9b86497449867dbb116bddb404edf3e733d70816
@Nekmurodov <nekmurodovdilshodbek@gmail.com>
2025-01-10 10:11:28 +0500

    first commit

---
+++ src/main/java/org/example/botoutlay/BotOutlayApplication.java
@@ -0,0 +1,13 @@
+package org.example.botoutlay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotOutlayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotOutlayApplication.class, args);
+	}
+
+}
